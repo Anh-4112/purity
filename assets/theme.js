ee9332fdc3115135a348c84e7b8cda01b5712b51
YounglyBlueskytechco-new <lypham.ac@gmail.com>
2025-03-13 15:39:26 +0700

    Style Header + bluid css

--- assets/theme.js
+++ assets/theme.js
@@ -1,12 +1,11 @@
-import { initSlide } from './module_slide.js?v=1';
+import { initSlide } from "./module_slide.js?v=1";
 
 var Shopify = Shopify || {};
 var root = document.getElementsByTagName("html")[0];
 var body = document.getElementsByTagName("body")[0];
 
-
-function eventModal(event){
-  if (event == 'open') {
+function eventModal(event) {
+  if (event == "open") {
     root.classList.add("open-modal");
   } else {
     root.classList.remove("open-modal");
@@ -40,18 +39,23 @@ class SlideSection extends HTMLElement {
 customElements.define("slide-section", SlideSection);
 
 class ToggleMenu extends HTMLElement {
-  constructor(){
+  constructor() {
     super();
     this.container = this.closest(".header__inner");
     this.init();
   }
-  init(){
-    this.addEventListener("click",  this.onClick.bind(this), false);
-    this.container.addEventListener('keyup', (event) => event.code.toUpperCase() === 'ESCAPE' && this.close());
+  init() {
+    this.addEventListener("click", this.onClick.bind(this), false);
+    this.container.addEventListener(
+      "keyup",
+      (event) => event.code.toUpperCase() === "ESCAPE" && this.close()
+    );
   }
-  onClick(e){
+  onClick(e) {
     e.preventDefault();
-    const menu_drawer = this.closest('.header-wrapper').querySelector("template.menu-drawer");
+    const menu_drawer = this.closest(".header-wrapper").querySelector(
+      "template.menu-drawer"
+    );
     if (menu_drawer) {
       const content = document.createElement("div");
       content.appendChild(
@@ -60,104 +64,131 @@ class ToggleMenu extends HTMLElement {
       body.appendChild(content.querySelector("menu-drawer"));
       menu_drawer.remove();
     }
-    eventModal('open');
+    eventModal("open");
   }
 }
-customElements.define('toggle-menu', ToggleMenu);
+customElements.define("toggle-menu", ToggleMenu);
 
 class ModalOverlay extends HTMLElement {
-  constructor(){
+  constructor() {
     super();
     this.init();
   }
-  init(){
-    this.addEventListener("click",  this.onClick.bind(this), false);
+  init() {
+    this.addEventListener("click", this.onClick.bind(this), false);
   }
-  onClick(e){
-    eventModal('close');
+  onClick(e) {
+    eventModal("close");
   }
 }
-customElements.define('modal-overlay', ModalOverlay);
+customElements.define("modal-overlay", ModalOverlay);
 
 class MenuDrawer extends HTMLElement {
-  constructor(){
+  constructor() {
     super();
     this.close = this.querySelector(".modal__close");
     this.init();
   }
-  init(){
-    this.close.addEventListener("click",  this.onClick.bind(this), false);
+  init() {
+    this.close.addEventListener("click", this.onClick.bind(this), false);
   }
-  onClick(e){
-    eventModal('close');
+  onClick(e) {
+    eventModal("close");
   }
 }
-customElements.define('menu-drawer', MenuDrawer);
+customElements.define("menu-drawer", MenuDrawer);
 
-const megaMenuCount = new WeakMap;
+const megaMenuCount = new WeakMap();
 class DetailsMegaMenu extends HTMLDetailsElement {
   constructor() {
     super(),
-    this.summaryElement = this.firstElementChild,
-    this.contentElement = this.lastElementChild,
-    this._open = this.hasAttribute("open"),
-    this.header = document.querySelector('header'),
-    this.summaryElement.addEventListener("click", this.onSummaryClicked.bind(this)),
-    this.detectClickOutsideListener = this.detectClickOutside.bind(this),
-    this.detectEscKeyboardListener = this.detectEscKeyboard.bind(this),
-    this.detectFocusOutListener = this.detectFocusOut.bind(this),
-    this.detectHoverListener = this.detectHover.bind(this),
-    this.addEventListener("mouseenter", this.detectHoverListener.bind(this)),
-    this.addEventListener("mouseleave", this.detectHoverListener.bind(this))
+      (this.summaryElement = this.firstElementChild),
+      (this.contentElement = this.lastElementChild),
+      (this._open = this.hasAttribute("open")),
+      (this.header = document.querySelector("header")),
+      this.summaryElement.addEventListener(
+        "click",
+        this.onSummaryClicked.bind(this)
+      ),
+      (this.detectClickOutsideListener = this.detectClickOutside.bind(this)),
+      (this.detectEscKeyboardListener = this.detectEscKeyboard.bind(this)),
+      (this.detectFocusOutListener = this.detectFocusOut.bind(this)),
+      (this.detectHoverListener = this.detectHover.bind(this)),
+      this.addEventListener("mouseenter", this.detectHoverListener.bind(this)),
+      this.addEventListener("mouseleave", this.detectHoverListener.bind(this));
   }
   set open(value) {
-    value !== this._open && (this._open = value,
-    this.isConnected ? this.transition(value) : value ? this.setAttribute("open", "") : this.removeAttribute("open"))
+    value !== this._open &&
+      ((this._open = value),
+      this.isConnected
+        ? this.transition(value)
+        : value
+        ? this.setAttribute("open", "")
+        : this.removeAttribute("open"));
   }
   get open() {
-    return this._open
+    return this._open;
   }
   get menuTrigger() {
-    return this.header.hasAttribute("data-menu-trigger") ? this.header.getAttribute("data-menu-trigger") : "click"
+    return this.header.hasAttribute("data-menu-trigger")
+      ? this.header.getAttribute("data-menu-trigger")
+      : "click";
   }
   onSummaryClicked(event) {
     event.preventDefault(),
-    this.menuTrigger === "hover" && this.summaryElement.hasAttribute("data-href") && this.summaryElement.getAttribute("data-href").length > 0 ? window.location.href = this.summaryElement.getAttribute("data-href") : this.open = !this.open
+      this.menuTrigger === "hover" &&
+      this.summaryElement.hasAttribute("data-href") &&
+      this.summaryElement.getAttribute("data-href").length > 0
+        ? (window.location.href = this.summaryElement.getAttribute("data-href"))
+        : (this.open = !this.open);
   }
   async transition(value) {
-    return value ? (megaMenuCount.set(DetailsMegaMenu, megaMenuCount.get(DetailsMegaMenu) + 1),
-    this.setAttribute("open", ""),
-    this.summaryElement.setAttribute("open", ""),
-    setTimeout( () => this.contentElement.setAttribute("open", ""), 100),
-    document.addEventListener("click", this.detectClickOutsideListener),
-    document.addEventListener("keydown", this.detectEscKeyboardListener),
-    document.addEventListener("focusout", this.detectFocusOutListener),
-    this.classList.add("open-submenu")) : (megaMenuCount.set(DetailsMegaMenu, megaMenuCount.get(DetailsMegaMenu) - 1),
-    this.summaryElement.removeAttribute("open"),
-    this.contentElement.removeAttribute("open"),
-    document.removeEventListener("click", this.detectClickOutsideListener),
-    document.removeEventListener("keydown", this.detectEscKeyboardListener),
-    document.removeEventListener("focusout", this.detectFocusOutListener),
-    this.classList.remove("open-submenu"),
-    this.open || setTimeout( () => this.removeAttribute("open"), 400)
-  )}
+    return value
+      ? (megaMenuCount.set(
+          DetailsMegaMenu,
+          megaMenuCount.get(DetailsMegaMenu) + 1
+        ),
+        this.setAttribute("open", ""),
+        this.summaryElement.setAttribute("open", ""),
+        setTimeout(() => this.contentElement.setAttribute("open", ""), 100),
+        document.addEventListener("click", this.detectClickOutsideListener),
+        document.addEventListener("keydown", this.detectEscKeyboardListener),
+        document.addEventListener("focusout", this.detectFocusOutListener),
+        this.classList.add("open-submenu"))
+      : (megaMenuCount.set(
+          DetailsMegaMenu,
+          megaMenuCount.get(DetailsMegaMenu) - 1
+        ),
+        this.summaryElement.removeAttribute("open"),
+        this.contentElement.removeAttribute("open"),
+        document.removeEventListener("click", this.detectClickOutsideListener),
+        document.removeEventListener("keydown", this.detectEscKeyboardListener),
+        document.removeEventListener("focusout", this.detectFocusOutListener),
+        this.classList.remove("open-submenu"),
+        this.open || setTimeout(() => this.removeAttribute("open"), 400));
+  }
   detectClickOutside(event) {
-    !this.contains(event.target) && !(event.target.closest("details")instanceof DetailsMegaMenu) && (this.open = !1)
+    !this.contains(event.target) &&
+      !(event.target.closest("details") instanceof DetailsMegaMenu) &&
+      (this.open = !1);
   }
   detectEscKeyboard(event) {
     if (event.code === "Escape") {
       const targetMenu = event.target.closest("details[open]");
-      targetMenu && (targetMenu.open = !1)
+      targetMenu && (targetMenu.open = !1);
     }
   }
   detectFocusOut(event) {
-    event.relatedTarget && !this.contains(event.relatedTarget) && (this.open = !1)
+    event.relatedTarget &&
+      !this.contains(event.relatedTarget) &&
+      (this.open = !1);
   }
   detectHover(event) {
-    this.menuTrigger !== "hover" || (event.type === "mouseenter" ? this.open = !0 : this.open = !1)
+    this.menuTrigger !== "hover" ||
+      (event.type === "mouseenter" ? (this.open = !0) : (this.open = !1));
   }
 }
 customElements.define("details-mega-menu", DetailsMegaMenu, {
-  extends: "details"
+  extends: "details",
 }),
-megaMenuCount.set(DetailsMegaMenu, 0);
+  megaMenuCount.set(DetailsMegaMenu, 0);
