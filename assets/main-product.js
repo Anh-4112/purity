8d181d0d8f1a4ac4060f4a2f225fd18b0647a34e
Hoàng Huân <hoanghuan301095@gmail.com>
2025-03-17 16:50:55 +0700

    update code product detail

--- assets/main-product.js
+++ assets/main-product.js
@@ -1,6 +1,40 @@
-import { initSlide } from './module_slide.js';
+document.addEventListener("DOMContentLoaded", function () {
+  function addProductEntry(productJson, storedProducts) {
+    if (storedProducts === null) storedProducts = [];
+    const currentProductID = productJson.id.toString();
+    if (!storedProducts.includes(currentProductID)) {
+      storedProducts.unshift(currentProductID);
+      if (storedProducts.length > 25) {
+        storedProducts.pop();
+      }
+      localStorage.setItem(
+        "recently-viewed-products",
+        JSON.stringify(storedProducts)
+      );
+    } else {
+      const index = storedProducts.indexOf(currentProductID);
+      if (index > -1) {
+        storedProducts.splice(index, 1);
+      }
+      storedProducts.unshift(currentProductID);
+      localStorage.setItem(
+        "recently-viewed-products",
+        JSON.stringify(storedProducts)
+      );
+    }
+  }
 
-class ProductRecommendations extends HTMLElement {
+  const prodData = document.querySelector("[data-product-json]");
+  if (prodData != null) {
+    const productJson = JSON.parse(prodData.innerHTML);
+    const storedProducts = JSON.parse(
+      localStorage.getItem("recently-viewed-products")
+    );
+    addProductEntry(productJson, storedProducts);
+  }
+});
+
+class ProductRecommendations extends SlideSection {
   constructor() {
     super();
   }
@@ -20,20 +54,8 @@ class ProductRecommendations extends HTMLElement {
           if (recommendations && recommendations.innerHTML.trim().length) {
             this.innerHTML = recommendations.innerHTML;
           }
-          if (recommendations.innerHTML.trim().length === 0) {
-            this.remove();
-            if (document.querySelector(".product-recommendations-heading")) {
-              document
-                .querySelector(".product-recommendations-heading")
-                .remove();
-            }
-          }
         })
-        .finally(() => {
-          if (this.querySelector(".swiper-wrapper")) {
-            initSlide(this);
-          }
-        })
+        .finally(() => {})
         .catch((e) => {
           console.error(e);
         });
@@ -45,3 +67,63 @@ class ProductRecommendations extends HTMLElement {
   }
 }
 customElements.define("product-recommendations", ProductRecommendations);
+
+class CollapsibleRowDetails extends HTMLDetailsElement {
+  constructor() {
+    super(),
+      (this.summaryElement = this.firstElementChild),
+      (this.contentElement = this.lastElementChild),
+      this._open = this.hasAttribute("open"),
+      this.content = this.querySelector(".collapsible-row__content"),
+      this.summaryElement.addEventListener(
+        "click",
+        this.onSummaryClicked.bind(this)
+      );
+  }
+
+  get open() {
+    return this._open;
+  }
+
+  set open(value) {
+    value !== this._open &&
+      ((this._open = value),
+      this.isConnected
+        ? this.transition(value)
+        : value
+        ? this.setAttribute("open", "")
+        : this.removeAttribute("open"));
+  }
+
+  onSummaryClicked(event) {
+    event.preventDefault(),
+    this.open = !this.open;
+  }
+
+  async transition(value) {
+    return value
+      ? (Motion.animate(
+        this.content,
+        true ? { height: "auto"} : { height: 0 },
+        { duration: 0.3 } ),
+        this.setAttribute("open", ""))
+      : (Motion.animate(
+        this.content,
+        false ? { height: "auto"} : { height: 0 },
+        { duration: 0.3 } ),
+        this.removeAttribute("open"))
+  }
+}
+customElements.define("collapsible-row", CollapsibleRowDetails, {
+  extends: "details"
+});
+
+class StickyAddCart extends HTMLElement {
+  constructor() {
+    super();
+    this.init();
+  }
+  init() {
+  }
+}
+customElements.define("sticky-add-cart", StickyAddCart);
