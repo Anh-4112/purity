35bf579c79fb95786e493d4652a2c8233504425c
YounglyBlueskytechco-new <lypham.ac@gmail.com>
2025-03-22 11:18:47 +0700

    {style} product

--- assets/module_slide.js
+++ assets/module_slide.js
@@ -104,6 +104,7 @@ function initSlideMedia(_this, gallery, thumbnail) {
     : "horizontal";
   if (gallery == "thumbnail" && direction == "vertical") {
     direction = "vertical";
+    itemMobile = "auto";
   } else if (gallery == "main" || gallery == "gird") {
     direction = "horizontal";
   }
@@ -118,6 +119,7 @@ function initSlideMedia(_this, gallery, thumbnail) {
       itemMobile = 1.3;
     }
   }
+
   console.log(direction);
   const swiperSlide = new Swiper(swiperElement, {
     slidesPerView: itemMobile,
