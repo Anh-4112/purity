2c25a9a28fd0830c3ccb035451918459a2bcd45b
khiemphamm <khiempv1404@gmail.com>
2025-04-12 01:06:31 +0700

    STS3-393 Add testimonials section with customizable settings and swiper functionality

---
+++ assets/section-testimonials.js
@@ -0,0 +1,41 @@
+import { SlideSection } from "module-slide";
+
+class TestimonialsSlide extends SlideSection {
+  constructor() {
+    super();
+    this.initThumb();
+  }
+
+  initThumb() {
+    const sectionId = this.dataset.sectionId;
+    const thumbsContainer = document.querySelector(
+      `#testimonials-thumb-swiper-${sectionId}`
+    );
+    if (!thumbsContainer || !this.swiper) return;
+
+    if (!thumbsContainer.classList.contains('swiper-container')) {
+      thumbsContainer.classList.add('swiper-container');
+    }
+    this.thumbsSwiper = new Swiper(thumbsContainer, {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      watchSlidesProgress: true,
+      grabCursor: true,
+      allowTouchMove: true,
+      slideActiveClass: 'swiper-slide-thumb-active',
+      breakpoints: {
+        576: {
+          slidesPerView: "auto"
+        }
+      },
+    });
+
+    this.swiper.thumbs.swiper = this.thumbsSwiper;
+    this.swiper.thumbs.init();
+    
+    this.swiper.on('slideChangeTransitionEnd', () => {
+      this.swiper.thumbs.update();
+    });
+  }
+}
+customElements.define("testimonial-slide", TestimonialsSlide);
