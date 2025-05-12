c9bbdf3d039fc61584ce4a900b4d88c514e58334
hieuche123 <29031211hieuche123@users.noreply.github.com>
2025-05-12 13:47:12 +0700

    update-validate

--- assets/section-newsletter-popup.js
+++ assets/section-newsletter-popup.js
@@ -107,65 +107,6 @@ class NewsletterPopup extends HTMLElement {
       new LazyLoader(".image-lazy-load");
     }, timeShowPopup);
 
-    const form = document.querySelector('.newsletter__form--popup');
-    const submitButton = form.querySelector('.button-newletter-popup'); 
-    const passwordInput = form.querySelector('.newsletter-form-popup-input'); 
-    if (!form || !submitButton || !passwordInput) {
-      return;
-    }
-
-    const validateEmail = (value) => {
-      if (value.trim() === "") {
-        return window.validate.email_invalid;
-      }
-      const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-      if (!emailPattern.test(value)) {
-        return window.validate.emailRegex;
-      }
-
-      return ''; 
-    };
-
-    submitButton.addEventListener('click', function (e) {
-      const emailErrorMessage = validateEmail(passwordInput.value);
-      
-      if (emailErrorMessage) {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        showError(passwordInput, emailErrorMessage);
-        return false;
-      }
-
-      const spinner = submitButton?.querySelector(".icon-load");
-      const text = submitButton?.querySelector(".hidden-on-load.transition-short");
-
-      submitButton?.classList.add("loading");
-      spinner?.classList.remove("opacity-0", "pointer-none");
-      text?.classList.add("opacity-0");
-    });
-
-        function showError(input, message) {
-      const parentElement = input.closest(".field.relative");
-
-      let errorEl = parentElement.querySelector(".form-error-message");
-      if (!errorEl) {
-        errorEl = document.createElement("div");
-        errorEl.className = "form-error-message warning mt-10 max-w-custom fs-small inline-flex align-center gap-10 w-full h-custom";
-        errorEl.style.setProperty('--height', '5.6rem');
-        parentElement.insertAdjacentElement('beforeend', errorEl);
-      }
-
-      errorEl.innerHTML = `
-        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M10 7.5V11.6667" stroke="#79571b" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round"/>
-          <path d="M9.99962 17.8414H4.94962C2.05795 17.8414 0.849619 15.7747 2.24962 13.2497L4.84962 8.56641L7.29962 4.16641C8.78295 1.49141 11.2163 1.49141 12.6996 4.16641L15.1496 8.57474L17.7496 13.2581C19.1496 15.7831 17.933 17.8497 15.0496 17.8497H9.99962V17.8414Z" stroke="#79571b" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round"/>
-          <path d="M9.99683 14.167H10.0043" stroke="#79571b" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-        <span>${message}</span>
-      `;
-      input.classList.add('input-error'); 
-    }
-
     this.initNotShow(wrapper);
   }
 
